d55d5a039431a2c4a2ffaf4551e84bb05be2132e
xkurz <151745596+xkurz@users.noreply.github.com>
2024-10-07 23:37:40 -0800

    Update create.blade.php

--- Hrd_app2/resources/views/employee/create.blade.php
+++ Hrd_app2/resources/views/employee/create.blade.php
@@ -49,6 +49,21 @@
                 </div>
             </form>
         </div>
+        @if(session('success'))
+            <div class="mt-4 p-4 bg-green-200 text-green-800 rounded">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @if($errors->any())
+            <div class="mt-4 p-4 bg-red-200 text-red-800 rounded">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 </body>
 </html>
